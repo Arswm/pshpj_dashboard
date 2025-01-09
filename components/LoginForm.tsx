d1f27312ced10af930a331597973403d06b5b014
Arsawm <arsamsj242@gmail.com>
2025-01-09 12:03:46 +0330

    first commit

---
+++ components/LoginForm.tsx
@@ -0,0 +1,203 @@
+'use client';
+
+import React, {useState} from 'react';
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import {useForm, FieldValues} from "react-hook-form";
+import {emailRegex, mobileRegex} from "@/constants/regex";
+import Timer from "@/components/ui/Timer"
+
+import {
+    InputOTP,
+    InputOTPGroup, InputOTPSeparator,
+    InputOTPSlot,
+} from "@/components/ui/input-otp"
+import {REGEXP_ONLY_DIGITS_AND_CHARS} from "input-otp";
+
+
+function LoginForm() {
+    const [activeTab, setActiveTab] = useState<'mobile' | 'email'>('mobile');
+    const [otp, setOtp] = useState<boolean>(false);
+
+    const Api = process.env.NEXT_PUBLIC_API_URL
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+        reset
+    } = useForm();
+
+    // this is submitting of number logic
+    const onSubmit = async (data: FieldValues) => {
+        const formattedData = {
+            username: data.username,
+            type: 'login'
+        };
+
+        const response = await fetch(`${Api}/auth/otp`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formattedData)
+        })
+
+        const apiResponse = await response.json();
+
+        if (apiResponse.success) {
+            setOtp(true)
+        }
+
+        reset()
+    };
+
+
+    // this is submitting of otp logic
+    const onOtpSubmit = async (data: FieldValues) => {
+        console.log(data);
+    }
+
+    return (
+        <div className="bg-[#F1F5F9] w-full min-h-dvh flex justify-center items-center">
+            <div className="min-w-[500px] flex rounded border">
+                <div className="w-full p-6 bg-white md:p-8 rounded-s flex flex-col rounded">
+
+                    {otp ?
+                        <div>
+
+                            <p className={'text-center mx-auto mb-4'}>
+                                لطفا کد ۶ رقمی ارسال شده را وارد کنید
+                            </p>
+
+                            <form onSubmit={handleSubmit(onOtpSubmit)}>
+                                <InputOTP
+                                    dir={'ltr'}
+                                    maxLength={6}
+                                    pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+                                >
+                                    <InputOTPGroup className={'flex-row-reverse mx-auto'}>
+                                        <InputOTPSlot index={0}/>
+                                        <InputOTPSeparator className={'opacity-0'}/>
+                                        <InputOTPSlot index={1}/>
+                                        <InputOTPSeparator className={'opacity-0'}/>
+                                        <InputOTPSlot index={2}/>
+                                        <InputOTPSeparator className={'opacity-0'}/>
+                                        <InputOTPSlot index={3}/>
+                                        <InputOTPSeparator className={'opacity-0'}/>
+                                        <InputOTPSlot index={4}/>
+                                        <InputOTPSeparator className={'opacity-0'}/>
+                                        <InputOTPSlot index={5}/>
+                                    </InputOTPGroup>
+                                </InputOTP>
+                            </form>
+
+                            <Timer initialTime={120}/>
+                            <div className={'flex justify-center'}>
+                                <a href={'/login'}
+                                   className={'text-center mt-7 text-slate-400  py-0.5 px-4 rounded cursor-pointer'}>
+                                    ویرایش شماره تماس
+                                </a>
+                            </div>
+
+                        </div>
+                        : <>
+                            <h1 className="text-3xl font-semibold mb-3 text-center">خوش آمدید</h1>
+                            <p className="text-sm mb-6 text-center">
+                                سامانه اتوماسیون شرکت پردیس شگفت پارس جنوب
+                            </p>
+                            <Tabs
+                                defaultValue="mobile"
+                                onValueChange={(value) => setActiveTab(value as 'mobile' | 'email')}
+                            >
+                                <TabsList className="w-full mb-3">
+                                    <TabsTrigger value="mobile" className="w-1/2 h-full">
+                                        موبایل
+                                    </TabsTrigger>
+                                    <TabsTrigger value="email" className="w-1/2 h-full">
+                                        ایمیل
+                                    </TabsTrigger>
+                                </TabsList>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="flex flex-col gap-2 mb-4">
+                                        <TabsContent value="mobile">
+                                            {activeTab === 'mobile' && (
+                                                <Input
+                                                    {...register('username', {
+                                                        required: 'شماره موبایل نباید خالی باشد',
+                                                        pattern: {
+                                                            value: mobileRegex,
+                                                            message: 'شماره موبایل معتبر نیست',
+                                                        },
+                                                    })}
+                                                    placeholder="09123456789"
+                                                    className="placeholder:text-gray-300"
+                                                />
+                                            )}
+                                            {activeTab === 'mobile' && errors.username && (
+                                                <p className="text-red-400 text-start self-end">
+                                                    {errors.username.message as string}
+                                                </p>
+                                            )}
+                                        </TabsContent>
+                                        <TabsContent value="email">
+                                            {activeTab === 'email' && (
+                                                <Input
+                                                    {...register('username', {
+                                                        required: 'ایمیل نباید خالی باشد',
+                                                        pattern: {
+                                                            value: emailRegex,
+                                                            message: 'ایمیل معتبر نیست',
+                                                        },
+                                                    })}
+                                                    placeholder="email@example.com"
+                                                    className="placeholder:text-gray-300"
+                                                />
+                                            )}
+                                            {activeTab === 'email' && errors.username && (
+                                                <p className="text-red-400 text-start self-end">
+                                                    {errors.username.message as string}
+                                                </p>
+                                            )}
+                                        </TabsContent>
+                                    </div>
+                                    <Button disabled={isSubmitting} variant="primary" className="w-full mt-2">
+                                        {isSubmitting ? (
+                                            <div className="flex items-center justify-center gap-2">
+                                                <span>... در حال ارسال</span>
+                                                <svg
+                                                    className="animate-spin h-5 w-5 text-white"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <circle
+                                                        className="opacity-25"
+                                                        cx="12"
+                                                        cy="12"
+                                                        r="10"
+                                                        stroke="currentColor"
+                                                        strokeWidth="4"
+                                                    ></circle>
+                                                    <path
+                                                        className="opacity-75"
+                                                        fill="currentColor"
+                                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                                    ></path>
+                                                </svg>
+                                            </div>
+                                        ) : (
+                                            'ارسال'
+                                        )}
+                                    </Button>
+                                </form>
+                            </Tabs>
+                        </>}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
---
+++ components/ui/Timer.tsx
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+
+interface TimerProps {
+    initialTime: number;
+}
+
+const Timer: React.FC<TimerProps> = ({initialTime}) => {
+    const [time, setTime] = useState<number>(initialTime);
+
+    useEffect(() => {
+        if (time > 0) {
+            const timerId = setInterval(() => {
+                setTime((prevTime) => {
+                    if (prevTime <= 1) {
+                        clearInterval(timerId);
+                        return 0;
+                    }
+                    return prevTime - 1;
+                });
+            }, 1000);
+
+            return () => clearInterval(timerId);
+        }
+    }, [time]);
+
+    return (
+        <p>
+
+            <span className={''}>
+                {time > 0 ?
+                    <p className={'text-center mt-6'}>
+                        زمان باقی مانده تا انقضای کد <span
+                        className={'text-red-500 font-bold border border-red-500 rounded py-0.5 px-4'}>{time}</span> ثانیه
+                        میباشد
+                    </p>
+                    :
+                    <p className={'text-center text-green-600 border border-green-600 w-fit ms-auto py-0.5 px-4 rounded cursor-pointer mx-auto mt-6 hover:bg-green-600 hover:text-white duration-200'}>
+                        ارسال مجدد کد تایید
+                    </p>
+                }
+            </span>
+
+        </p>
+    );
+};
+
+export default Timer;
